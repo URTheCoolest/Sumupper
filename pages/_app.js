c7955bae2a6ed746d6e06d849dafff52aaabcb4b
URTheCoolest <Jobangles4life@gmail.com>
2025-10-17 06:57:18 +0200

    Web Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import NavigationBar from '../components/NavigationBar'
 import { LanguageProvider } from '../context/LanguageContext'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
       <NavigationBar />
       <Component {...pageProps} />
+      <Analytics />
     </LanguageProvider>
   )
 }
